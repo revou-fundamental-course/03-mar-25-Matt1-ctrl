ae55f3416def4e565615d36e46d3579cfa6f8485
Matt-ctrl <mamatppt@gmail.com>
2025-03-12 10:14:38 +0800

    update1

---
+++ Rahmatullah RevoU/js/script.js
@@ -0,0 +1,14 @@
+// Smooth Scroll untuk navigasi
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener("click", function(e) {
+        e.preventDefault();
+
+        const target = document.querySelector(this.getAttribute("href"));
+        if (target) {
+            window.scrollTo({
+                top: target.offsetTop - 50, // Sesuaikan dengan tinggi navbar
+                behavior: "smooth"
+            });
+        }
+    });
+});
